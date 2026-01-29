d12bd29535957acda11dfff0f445c2449d495d6d
Imdo714 <gusehdla@naver.com>
2026-01-29 22:01:20 +0900

    feat: MySQL 및 JPA 의존성 추가 및 설정
    `build.gradle.kts`에 MySQL 드라이버 추가 밑 `application.yml` DB 연결 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.2.20"
     kotlin("plugin.spring") version "2.2.20" // Kotlin 클래스에 자동으로 open 키워드를 추가 해줌
+    kotlin("plugin.jpa") version "2.2.20" // JPA 엔티티를 위한 기본 생성자 자동 생성
 
     id("org.springframework.boot") version "3.2.1" // 실행 가능한 Jar 빌드 및 Boot 환경 제공
     id("io.spring.dependency-management") version "1.1.4" // 라이브러리 버전 자동 관리
@@ -16,6 +17,10 @@ repositories {
 dependencies {
     // Spring Boot
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // Mysql
+    runtimeOnly("com.mysql:mysql-connector-j")
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
